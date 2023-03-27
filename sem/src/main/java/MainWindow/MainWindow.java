5d3388eaa471af124d00bd9577d52de71d42cc5d
Dmytro Kadar <kadardmy@fel.cvut.cz>
2023-03-27 23:11:23 +0200

    Added Window and GamePanel

---
+++ sem/src/main/java/MainWindow/MainWindow.java
@@ -0,0 +1,19 @@
+package MainWindow;
+
+import javax.swing.*;
+
+public class MainWindow extends JFrame {
+    public MainWindow() {
+        setTitle("Arimaa");
+
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setContentPane(new GamePanel(1280,720));
+        //TODO add picture
+//        setIconImage(new ImageIcon(imgURL).getImage());
+        setIgnoreRepaint(true);
+
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+}
